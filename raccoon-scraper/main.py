d3293914662525e5116e114252f54f49eb0e312c
Lucas Targa <lucasltarga@gmail.com>
2025-08-04 18:31:15 -0300

    Change page load strategy to none and save complaints as csv

--- raccoon-scraper/main.py
+++ raccoon-scraper/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import scraper
+import pandas as pd
 
 '''RAccoon Scraper: A web scraper for extracting complaints from Reclame Aqui.
 This script uses Selenium to navigate the Reclame Aqui website, extract complaint links,
@@ -12,10 +13,14 @@ def main():
     # Base URL for the company complaints on Reclame Aqui
     company_complaints_url = "https://www.reclameaqui.com.br/empresa/NOME_DA_EMPRESA/lista-reclamacoes/"
     
+    # Categories for the complaints. You can specify categories by their IDs.
+    categories = []
+
     # Generate URLs for pages 1 to 50 for the company complaints. 
     # Number of pages is limited to 50 as per the website's restrictions. 
     # For better results, consider scraping each category separately by passing a list of categories IDs as a parameter.
-    pages = scraper.generate_list_of_pages(company_complaints_url)
+    pages = scraper.generate_list_of_pages(company_complaints_url, categories)
+    links = []
 
     # Sample use
     # Loop through the first 5 pages to get complaint links and print them
@@ -23,27 +28,33 @@ def main():
     for page in pages[0:5]:
         # Initialize a new browser instance for each page to avoid session issues
         # This is necessary because Reclame Aqui may restrict automated access if the same session is used
+        options = webdriver.ChromeOptions()
+        options.page_load_strategy = "none"
+        browser = webdriver.Chrome(options=options)
         browser = webdriver.Chrome()
         browser.maximize_window()
         print(f"Getting links from {page}...")
         browser.get(page)
-        links = scraper.get_links(browser)
-        print(links)
+        links += scraper.get_links(browser)
         browser.close()
 
-    #Uncomment the following lines to test with specific complaint links
-    '''
-    links = []
     # Loop through each complaint link and extract main details
     # Browser is reopened for each complaint to avoid automation restrictions
+    complaints_data = []
+    
     for complaint in links:
-        browser = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.page_load_strategy = "none"
+        browser = webdriver.Chrome(options=options)
         browser.maximize_window()
         browser.get(complaint)
-        scraper.get_complaint_from_browser(browser)
+        complaint_data = scraper.get_complaint_from_browser(browser)
+        complaints_data.append(complaint_data)
         browser.close()
-    '''
 
+    df = pd.DataFrame(complaints_data)
+    df.to_csv("complaints_data.csv", index=False)
+    print("Data saved to complaints_data.csv")
     print("\nScraping done.")
 
 if __name__ == '__main__':
